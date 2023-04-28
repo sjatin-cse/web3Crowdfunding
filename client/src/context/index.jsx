4fda8c4f6123494c77ad26a29564d16d41a9fa2c
Jatin Sharma <sjatin.cse@gmail.com>
2023-04-29 03:36:59 +0530

    web3 app

---
+++ client/src/context/index.jsx
@@ -0,0 +1,119 @@
+import React, { useContext, createContext, useState } from "react";
+
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0x95e2c4A66e808B3825500c7Cc3cAc6C77d0FB623"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+  const [address, setAddress] = useState(useAddress());
+  // // setAddress(useAddress());
+  // var address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, // owner
+        form.title, // title
+        form.description, // description
+        form.target,
+        new Date(form.deadline).getTime(), // deadline,
+        form.image,
+      ]);
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+  const getCampaign = async () => {
+    const campaigns = await contract.call("getCampaign");
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+
+    return parsedCampaings;
+  };
+
+  const getUserCampaign = async () => {
+    const allCampaigns = await getCampaign();
+
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+
+    return filteredCampaigns;
+  };
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call("donateToCampaign", pId, {
+      value: ethers.utils.parseEther(amount),
+    });
+
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", pId);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
+    }
+
+    return parsedDonations;
+  };
+
+  const logout = () => setAddress(null);
+
+  return (
+    <StateContext.Provider
+      value={{
+        logout,
+        address,
+        contract,
+        setAddress,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaign,
+        getUserCampaign,
+        donate,
+        getDonations,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components/index";
+
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getCampaign } = useStateContext();
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaign();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Home;
